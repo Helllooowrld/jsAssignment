055aa28b1dda917fc119a9da008500a73f292369
Aayushya Shrestha <sthaayu333@gmail.com>
2025-01-19 09:40:44 +0545

    Added Comments

---
+++ exe1.js
@@ -0,0 +1,6 @@
+//Write a program that prints all even numbers from 1 to 50 using proper indentation and comments to explain your code.
+for(let i=1;i<=50;i++)
+{
+    i++ //increment i by 1 to initialise i to 2
+    console.log("The even numbers are: ",i) //print the even numbers
+}
