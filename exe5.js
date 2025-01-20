ad327beecb8a0d8a75d2f1b0499e94d8fdde13ea
Aayushya Shrestha <sthaayu333@gmail.com>
2025-01-20 19:40:06 +0545

    Fifth Question

--- exe5.js
+++ exe5.js
@@ -1,8 +1,8 @@
 var a=1 //var 
-let a=2 //No error because var is utilised
+var a=2 //No error because var is utilised and it allows redeclaration
 let b=4 //let
-var b=0 //erroe because let doesnt allow reinitialization
+//let b=0 //error because let doesnt allow redeclaration
 const c=3 //const
 a=5 //No error since var is used
 b=6 //no error since let is use
-c=5 //error since const is used
+//c=5 //error since const is used
