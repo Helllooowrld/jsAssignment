9f0ccb08c1c9b047c459b30db76b737672d2a193
Aayushya Shrestha <sthaayu333@gmail.com>
2025-01-20 18:43:31 +0545

    Third Question

---
+++ exe3.js
@@ -0,0 +1,12 @@
+let string="Hello" //string datatype
+let num=123 //number datatype
+let boolean=true    //boolean datatype
+let obj={word:"World"}  //object datatype
+let x   //undefined
+let a=null  //null
+console.log(typeof string)
+console.log(typeof num)
+console.log(typeof boolean)
+console.log(typeof obj)
+console.log( x)
+console.log(a)
